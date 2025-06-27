4737abc0b586091e469d1fdb802ee2203ce15a2f
寺胡玖已 <w1654395585@outlook.com>
2025-06-27 19:18:35 +0800

    Update 点我启动.bat

--- 点我启动.bat
+++ 点我启动.bat
@@ -3,10 +3,10 @@ REM 启动 data_fetcher.py 并将日志输出到 data_fetcher.log 文件
 start /B python data_fetcher.py > data_fetcher.log 2>&1
 
 REM 启动 config.pyw 以显示配置窗口
-start pythonw config.pyw
+start .venv\Scripts\pythonw.exe config.pyw
 
 REM 运行 c2dglab.py 并输出其日志到当前窗口
-uv run c2dglab.py
+.venv\Scripts\python.exe c2dglab.py
 
 REM 暂停，等待用户按任意键继续
 pause
